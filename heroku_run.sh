c49437fdfb8cb0c958be332dc53403889f555011
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2019-11-06 08:22:31 -0800

    change grafana url to grafana db

--- heroku_run.sh
+++ heroku_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export GF_SERVER_HTTP_PORT=$PORT
-export GF_DATABASE_URL=$DATABASE_URL
+export GF_DATABASE_URL=$GRAFANA_DATABASE_URL
 
 echo "Starting grafana on port $GF_SERVER_HTTP_PORT"
 /run.sh
