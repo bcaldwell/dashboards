3527096d607151c6d32a537700819d39d957fdbf
Benjamin Caldwell <8838361+bcaldwell@users.noreply.github.com>
2019-11-06 16:57:15 -0800

    Update heroku_run.sh

--- heroku_run.sh
+++ heroku_run.sh
@@ -3,5 +3,12 @@
 export GF_SERVER_HTTP_PORT=$PORT
 export GF_DATABASE_URL=$GRAFANA_DATABASE_URL
 
+if [[ $DATABASE_URL =~ ^(.+):(.+)@(.+)/(.+)$ ]]; then
+    export DATABASE_USER=${BASH_REMATCH[1]}
+    export DATABASE_PASSWORD=${BASH_REMATCH[2]}
+    export DATABASE_URL=${BASH_REMATCH[3]}
+    export DATABASE_DB=${BASH_REMATCH[4]}
+fi
+
 echo "Starting grafana on port $GF_SERVER_HTTP_PORT"
 /run.sh
